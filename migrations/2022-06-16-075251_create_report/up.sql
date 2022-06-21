b34b629517ecc80ad67fc08da33e36fe19a513b0
SLASHLogin <loginmlgxd@gmail.com>
2022-06-22 00:29:02 +0200

    Add default now() to the report's timestamp

--- migrations/2022-06-16-075251_create_report/up.sql
+++ migrations/2022-06-16-075251_create_report/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE public.report
     id uuid NOT NULL DEFAULT gen_random_uuid (),
     member_uuid uuid NOT NULL,
     content text NOT NULL,
-    create_date date NOT NULL,
+    create_date date NOT NULL DEFAULT now(),
     PRIMARY KEY (id),
     CONSTRAINT "FK_report_member" FOREIGN KEY (member_uuid)
         REFERENCES public.member (id) MATCH FULL
