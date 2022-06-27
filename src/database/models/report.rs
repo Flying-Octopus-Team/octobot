2f1ccc1226c43f5dfc4a59a93d4df3bc8f2cb984
SLASHLogin <loginmlgxd@gmail.com>
2022-06-27 12:16:56 +0200

    Add CRUD methods to the Report. Add AsChangeset trait.

--- src/database/models/report.rs
+++ src/database/models/report.rs
@@ -1,9 +1,11 @@
 use crate::database::schema::report;
+use crate::diesel::ExpressionMethods;
 use chrono::NaiveDate;
-use diesel::RunQueryDsl;
+use diesel::{QueryDsl, RunQueryDsl};
 use uuid::Uuid;
 
-#[derive(Queryable, Debug)]
+#[derive(Queryable, AsChangeset, Debug)]
+#[diesel(table_name = report)]
 struct Report {
     id: Uuid,
     member_uuid: Uuid,
@@ -18,14 +20,40 @@ struct NewReport {
     content: String,
 }
 
-fn create_report(member_uuid: Uuid, content: String) -> Report {
-    let new_report = NewReport {
-        member_uuid,
-        content,
-    };
+impl Report {
+    pub fn new(member_uuid: Uuid, content: String) -> Report {
+        Report {
+            id: Uuid::new_v4(),
+            member_uuid,
+            content,
+            create_date: chrono::Local::now().naive_local().date(),
+        }
+    }
 
-    diesel::insert_into(report::table)
-        .values(&new_report)
-        .get_result(&mut crate::database::PG_POOL.get().unwrap())
-        .expect("Error creating new report")
+    pub fn insert(member_uuid: Uuid, content: String) -> Report {
+        let new_report = NewReport {
+            member_uuid,
+            content,
+        };
+
+        diesel::insert_into(report::table)
+            .values(&new_report)
+            .get_result(&mut crate::database::PG_POOL.get().unwrap())
+            .expect("Error creating new report")
+    }
+
+    pub fn update(&self) -> Self {
+        diesel::update(report::table)
+            .set(self)
+            .get_result(&mut crate::database::PG_POOL.get().unwrap())
+            .expect("Error updating report")
+    }
+
+    pub fn delete(&self) -> bool {
+        use crate::database::schema::report::dsl::*;
+
+        diesel::delete(report.filter(id.eq(id)))
+            .execute(&mut crate::database::PG_POOL.get().unwrap())
+            .is_ok()
+    }
 }
