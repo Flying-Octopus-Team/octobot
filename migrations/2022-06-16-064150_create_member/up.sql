cc0857e9cb63f35dc4ee098694b5183bde83798b
SLASHLogin <loginmlgxd@gmail.com>
2022-07-09 13:11:39 +0200

    Make member's dc_id, trello_id, card_id unique

--- migrations/2022-06-16-064150_create_member/up.sql
+++ migrations/2022-06-16-064150_create_member/up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS public.member
 (
     id uuid NOT NULL DEFAULT gen_random_uuid (),
-    discord_id text,
-    trello_id text,
-    trello_report_card_id text,
+    discord_id text UNIQUE,
+    trello_id text UNIQUE,
+    trello_report_card_id text UNIQUE,
     PRIMARY KEY (id)
 );
