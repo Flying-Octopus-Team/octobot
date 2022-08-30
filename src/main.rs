e56af5338c942e351e1edc97685bb197fc346901
SLASHLogin <loginmlgxd@gmail.com>
2022-08-30 23:23:04 +0200

    Implement basic tracing setup with outputs to the std and logs/ directory

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ extern crate diesel;
 
 use lazy_static::lazy_static;
 use settings::Settings;
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::{
+    filter::{EnvFilter, LevelFilter},
+    fmt::Layer,
+    prelude::__tracing_subscriber_SubscriberExt,
+};
 
 mod database;
 mod discord;
@@ -14,6 +20,26 @@ lazy_static! {
 
 #[tokio::main]
 async fn main() {
+    let _guard = setup_tracing();
+
     database::run_migrations();
     discord::start_bot().await;
+}
+
+fn setup_tracing() -> WorkerGuard {
+    let dir = std::env::current_dir().unwrap().join("logs");
+
+    let file_appender = tracing_appender::rolling::daily(dir, "octobot.log");
+    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+
+    let mut layer = Layer::new().with_writer(non_blocking);
+    layer.set_ansi(false);
+    let subscriber = tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env().add_directive(LevelFilter::INFO.into()))
+        .with(Layer::new().pretty().with_writer(std::io::stdout))
+        .with(layer);
+
+    tracing::subscriber::set_global_default(subscriber).expect("Unable to set a global subscriber");
+
+    guard
 }
