156df72595bfa97f30433093775b5a820e8e772a
SLASHLogin <loginmlgxd@gmail.com>
2022-09-06 17:05:08 +0200

    Add `display_name` to the member model. Add from Discord id method

--- src/database/models/member.rs
+++ src/database/models/member.rs
@@ -5,15 +5,19 @@ use crate::database::schema::member;
 use crate::diesel::ExpressionMethods;
 use crate::diesel::RunQueryDsl;
 use crate::discord::find_option_as_string;
+use crate::SETTINGS;
 use diesel::QueryDsl;
 use diesel::Table;
 use serenity::model::application::interaction::application_command::CommandDataOption;
+use serenity::prelude::Context;
+use tracing::error;
 use uuid::Uuid;
 
 #[derive(Queryable, Identifiable, Insertable, AsChangeset, Debug)]
 #[diesel(table_name = member)]
 pub struct Member {
     id: Uuid,
+    display_name: String,
     discord_id: Option<String>,
     trello_id: Option<String>,
     trello_report_card_id: Option<String>,
@@ -103,6 +107,42 @@ impl Member {
 
     pub fn id(&self) -> Uuid {
         self.id
+    }
+
+    pub(crate) fn from_discord_id(
+        user_id: String,
+        ctx: &Context,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let member_id = match user_id.parse::<u64>() {
+            Ok(id) => id,
+            Err(_) => {
+                let error_msg = format!("Invalid member id: {}", user_id);
+                error!("{}", error_msg);
+                return Err(error_msg.into());
+            }
+        };
+        let guild_member = match ctx.cache.member(SETTINGS.server_id, member_id) {
+            Some(guild_member) => guild_member,
+            None => {
+                let error_msg = format!("Member not found in the guild: {}", member_id);
+                error!("{}", error_msg);
+                return Err(error_msg.into());
+            }
+        };
+
+        let result = match Member::find_by_discord_id(guild_member.user.id.to_string()) {
+            Ok(result) => result,
+            Err(why) => {
+                let error_msg = format!(
+                    "Member not found in database: {}\nReason: {}",
+                    member_id, why
+                );
+                error!("{}", error_msg);
+                return Err(error_msg.into());
+            }
+        };
+
+        Ok(result)
     }
 
     pub(crate) fn name(&self) -> String {
