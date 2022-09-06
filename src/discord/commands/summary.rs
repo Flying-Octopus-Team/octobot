2fc7be92f9b911754379c5bea13e232d263f698d
SLASHLogin <loginmlgxd@gmail.com>
2022-09-06 18:01:11 +0200

    Send summary to the channel

--- src/discord/commands/summary.rs
+++ src/discord/commands/summary.rs
@@ -1,17 +1,29 @@
+use serenity::prelude::Context;
 use tracing::info;
 
-use crate::database::models::report::Report;
+use crate::{database::models::report::Report, SETTINGS};
 
-pub(crate) async fn generate_summary() -> Result<String, Box<dyn std::error::Error>> {
+pub(crate) async fn generate_summary(ctx: &Context) -> Result<String, Box<dyn std::error::Error>> {
     info!("Generating summary");
 
-    let output = Report::report_summary(None).await?;
+    let summary = Report::report_summary(None).await?;
 
-    if output.is_empty() {
-        info!("No reports to summarize");
-        Ok("No unpublished reports".to_string())
+    if summary.is_empty() {
+        info!("Generated empty summary");
+        Ok("Summary is empty. Nothing was sent".to_string())
     } else {
-        info!("Summary generated");
-        Ok(output)
+        // send summary to the channel
+        let channel_id = SETTINGS.meeting.summary_channel;
+        match channel_id.send_message(&ctx.http, |m| m.content(summary)).await {
+            Ok(_) => {
+                info!("Generated summary and sent it to the channel");
+                Ok("Summary was sent".to_string())
+            }
+            Err(e) => {
+                let error = format!("Error sending summary to the channel: {:?}", e);
+                info!(?e, "Generated summary but failed to send it to the channel");
+                Err(error.into())
+            }
+        }
     }
 }
