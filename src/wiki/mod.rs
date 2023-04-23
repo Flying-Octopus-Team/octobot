c3095981434366a53c5b9b30fd9aa50d3b944e9c
SLASHLogin <loginmlgxd@gmail.com>
2023-04-23 14:13:06 +0200

    Add functions for un/assigning groups. Update deps

---
+++ src/wiki/mod.rs
@@ -0,0 +1,104 @@
+use graphql_client::GraphQLQuery;
+use reqwest::header;
+
+use crate::SETTINGS;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "wiki/schema.graphql",
+    query_path = "wiki/mutations/assign_user_group.graphql",
+    response_derives = "Debug"
+)]
+pub struct AssignUserGroup;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "wiki/schema.graphql",
+    query_path = "wiki/mutations/unassign_user_group.graphql",
+    response_derives = "Debug"
+)]
+pub struct UnassignUserGroup;
+
+pub async fn assign_user_group(
+    variables: assign_user_group::Variables,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut headers = header::HeaderMap::new();
+
+    let mut auth_value = header::HeaderValue::from_static(&SETTINGS.wiki.token);
+    auth_value.set_sensitive(true);
+    headers.insert(header::AUTHORIZATION, auth_value);
+
+    let client = reqwest::Client::builder()
+        .user_agent("octobot/".to_owned() + env!("CARGO_PKG_VERSION"))
+        .default_headers(headers)
+        .build()?;
+
+    let body = AssignUserGroup::build_query(variables);
+
+    let res = client.post(&SETTINGS.wiki.url).json(&body).send().await?;
+
+    let response_body: graphql_client::Response<assign_user_group::ResponseData> =
+        res.json().await?;
+
+    if response_body.errors.is_some() {
+        return Err(response_body.errors.unwrap()[0].message.clone().into());
+    }
+
+    let response_result = response_body
+        .data
+        .unwrap()
+        .groups
+        .unwrap()
+        .assign_user
+        .unwrap()
+        .response_result
+        .unwrap();
+
+    if response_result.succeeded {
+        Ok(())
+    } else {
+        Err(response_result.message.unwrap().into())
+    }
+}
+
+pub async fn unassign_user_group(
+    variables: unassign_user_group::Variables,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut headers = header::HeaderMap::new();
+
+    let mut auth_value = header::HeaderValue::from_static(&SETTINGS.wiki.token);
+    auth_value.set_sensitive(true);
+    headers.insert(header::AUTHORIZATION, auth_value);
+
+    let client = reqwest::Client::builder()
+        .user_agent("octobot/".to_owned() + env!("CARGO_PKG_VERSION"))
+        .default_headers(headers)
+        .build()?;
+
+    let body = UnassignUserGroup::build_query(variables);
+
+    let res = client.post(&SETTINGS.wiki.url).json(&body).send().await?;
+
+    let response_body: graphql_client::Response<unassign_user_group::ResponseData> =
+        res.json().await?;
+
+    if response_body.errors.is_some() {
+        return Err(response_body.errors.unwrap()[0].message.clone().into());
+    }
+
+    let response_result = response_body
+        .data
+        .unwrap()
+        .groups
+        .unwrap()
+        .unassign_user
+        .unwrap()
+        .response_result
+        .unwrap();
+
+    if response_result.succeeded {
+        Ok(())
+    } else {
+        Err(response_result.message.unwrap().into())
+    }
+}
