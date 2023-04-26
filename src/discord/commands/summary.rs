c41c5029f042d0857ddc9ed9752c6b1626d5d59b
SLASHLogin <loginmlgxd@gmail.com>
2023-04-26 18:35:13 +0200

    Port summary commands to poise

--- src/discord/commands/summary.rs
+++ src/discord/commands/summary.rs
@@ -1,58 +1,42 @@
-use serenity::{
-    model::prelude::interaction::application_command::CommandDataOption, prelude::Context,
-};
 use std::fmt::Write;
-use tracing::{info, log::error};
+use tracing::info;
 
-use crate::{
-    database::models::summary::Summary,
-    discord::{find_option_as_string, find_option_value},
-    meeting::MeetingStatus,
-};
+use crate::database::models::summary::Summary;
+use crate::discord::Context;
+use crate::discord::Error;
 
+#[poise::command(slash_command, rename = "preview")]
 pub(crate) async fn preview_summary(
-    ctx: &Context,
-    option: &CommandDataOption,
-) -> Result<String, Box<dyn std::error::Error>> {
+    ctx: Context<'_>,
+    #[description = "Preview summary by ID"] summary: Option<Summary>,
+    #[description = "Note to add to the summary"] note: Option<String>,
+) -> Result<(), Error> {
     info!("Generating summary preview");
 
-    let note = {
-        match find_option_as_string(&option.options[..], "note") {
-            Some(note) => note,
-            None => String::new(),
-        }
-    };
+    let note = note.unwrap_or_default();
 
-    let summary = if let Some(summary_id) = find_option_as_string(&option.options[..], "id") {
-        Summary::find_by_id(uuid::Uuid::parse_str(&summary_id)?)?
+    let summary = if let Some(summary) = summary {
+        summary
     } else {
-        let read = ctx.data.read().await;
-        let meeting_status = read.get::<MeetingStatus>().unwrap().clone();
-        let meeting_status = meeting_status.write().await;
+        let meeting_status = ctx.data().meeting_status.write().await;
 
         Summary::find_by_id(meeting_status.summary_id())?
     };
 
     let summary = summary.generate_summary(note, false).await?;
 
-    if summary.is_empty() {
-        info!("Generated empty summary");
-        Ok("Summary is empty".to_string())
-    } else {
-        Ok(summary)
-    }
+    crate::discord::respond(ctx, summary).await
 }
 
-pub(crate) fn list_summaries(
-    option: &CommandDataOption,
-) -> Result<String, Box<dyn std::error::Error>> {
+#[poise::command(slash_command, rename = "list")]
+pub(crate) async fn list_summaries(
+    ctx: Context<'_>,
+    #[description = "Page number"] page: Option<i64>,
+    #[description = "Page size"] page_size: Option<i64>,
+) -> Result<(), Error> {
     info!("Listing summaries");
 
-    let page =
-        find_option_value(&option.options[..], "page").map_or(1, |page| page.as_i64().unwrap());
-
-    let page_size = find_option_value(&option.options[..], "page-size")
-        .map(|page_size| page_size.as_i64().unwrap());
+    let page = page.unwrap_or(1);
 
     let (summaries, total_pages) = Summary::list(page, page_size)?;
 
@@ -63,41 +47,17 @@ pub(crate) fn list_summaries(
     }
     write!(output, "Page {}/{}", page, total_pages)?;
 
-    Ok(output)
+    crate::discord::respond(ctx, output).await
 }
 
+#[poise::command(slash_command, rename = "resend")]
 pub(crate) async fn resend_summary(
-    ctx: &Context,
-    option: &CommandDataOption,
-) -> Result<String, Box<dyn std::error::Error>> {
+    ctx: Context<'_>,
+    #[description = "ID of the summary to resend"] summary: Summary,
+) -> Result<(), Error> {
     info!("Resending summary");
-
-    let id = match find_option_as_string(&option.options[..], "id") {
-        Some(id) => match uuid::Uuid::parse_str(&id) {
-            Ok(id) => id,
-            Err(why) => {
-                let error_msg = format!("Wrong ID value. Could not parse the value: {}", why);
-                error!("{}", error_msg);
-                return Err(error_msg.into());
-            }
-        },
-        None => {
-            let error_msg = "Didn't find ID option in the command".to_string();
-            error!("{}", error_msg);
-            return Err(error_msg.into());
-        }
-    };
-
-    let summary = match Summary::find_by_id(id) {
-        Ok(summary) => summary,
-        Err(why) => {
-            let error_msg = format!("There's no summary with such ID: {id}. Error: {why}");
-            error!("{}", error_msg);
-            return Err(error_msg.into());
-        }
-    };
 
     let output = summary.send_summary(ctx, true).await?;
 
-    Ok(output)
+    crate::discord::respond(ctx, output).await
 }
