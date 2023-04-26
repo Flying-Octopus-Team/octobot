c061e7fe0fc3187e4dbd63e9911ded7d1c399732
SLASHLogin <loginmlgxd@gmail.com>
2023-04-26 18:32:49 +0200

    Port report commands to poise

--- src/discord/commands/report.rs
+++ src/discord/commands/report.rs
@@ -1,54 +1,35 @@
-use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
-use serenity::model::application::interaction::application_command::CommandDataOption;
-use serenity::prelude::Context;
 use std::fmt::Write;
-use tracing::info;
-use uuid::Uuid;
 
+use tracing::error;
+use tracing::info;
+
+use super::Error;
 use crate::database::models::summary::Summary;
 use crate::database::models::{member::Member, report::Report};
-use crate::discord::find_option_as_string;
+use crate::discord::Context;
 
-use super::find_option_value;
-
+#[poise::command(slash_command, aliases("add"))]
 pub(crate) async fn add_report(
-    ctx: &Context,
-    command: &ApplicationCommandInteraction,
-    option: &CommandDataOption,
-) -> Result<String, Box<dyn std::error::Error>> {
+    ctx: Context<'_>,
+    #[description = "Report's content"] content: String,
+    #[description = "Member's ID"] member: Option<Member>,
+    #[description = "Summary's ID"] summary: Option<Summary>,
+) -> Result<(), Error> {
     info!("Adding report");
-    let member_dc_id = match find_option_value(&option.options[..], "member") {
-        Some(member_id) => String::from(member_id.as_str().unwrap()),
-        None => command
-            .member
-            .as_ref()
-            .unwrap()
-            .user
-            .id
-            .as_u64()
-            .to_string(),
-    };
-
-    let member = match Member::find_by_discord_id(member_dc_id) {
-        Ok(member) => member,
-        Err(why) => return Ok(format!("Member not found in the database: {}", why)),
-    };
-
-    let content = match find_option_value(&option.options[..], "content") {
-        Some(content) => String::from(content.as_str().unwrap()),
-        None => return Ok("No content specified".to_string()),
-    };
-
-    let summary = match find_option_as_string(&option.options[..], "summary") {
-        Some(meeting) => Some(uuid::Uuid::parse_str(&meeting)?),
-        None => None,
+    let member = match member {
+        Some(member) => member,
+        None => {
+            let author_id = ctx.author().id.to_string();
+            match Member::find_by_discord_id(author_id) {
+                Ok(member) => member,
+                Err(why) => return Err(anyhow!("Member not found in the database: {}", why)),
+            }
+        }
     };
 
     let mut report = Report::insert(member.id(), content)?;
 
     if let Some(summary) = summary {
-        let summary = Summary::find_by_id(summary)?;
-
         report.set_summary_id(summary.id())?;
 
         if summary.is_published() {
@@ -60,59 +41,49 @@ pub(crate) async fn add_report(
 
     info!("Report added: {:?}", report);
 
-    Ok(format!("Report added: {}", report))
+    let output = format!("Report added: {}", report);
+
+    crate::discord::respond(ctx, output).await
 }
 
-pub(crate) fn remove_report(
-    option: &CommandDataOption,
-) -> Result<String, Box<dyn std::error::Error>> {
+#[poise::command(slash_command, rename = "remove")]
+pub(crate) async fn remove_report(
+    ctx: Context<'_>,
+    #[description = "Report's ID"] report: Report,
+) -> Result<(), Error> {
     info!("Removing report");
-    let report_id = match find_option_value(&option.options[..], "id") {
-        Some(report_id) => Uuid::parse_str(report_id.as_str().unwrap()),
-        None => return Ok("No report specified".to_string()),
-    }?;
 
-    let report = match Report::find_by_id(report_id) {
-        Ok(report) => report,
-        Err(why) => return Ok(format!("Can't find report with this ID: {why}")),
-    };
+    let mut output = String::new();
 
     match report.delete() {
         Ok(deleted) => match deleted {
             true => {
                 info!("Report removed: {:?}", report);
-                Ok("Report deleted".to_string())
+                writeln!(&mut output, "Report removed: {}", report)?;
             }
             false => {
                 info!("Removed 0 reports");
-                Ok("Deleted 0 reports".to_string())
+                writeln!(&mut output, "Removed 0 reports")?;
             }
         },
-        Err(err) => Err(err),
-    }
-}
-
-pub(crate) fn list_reports(
-    option: &CommandDataOption,
-) -> Result<String, Box<dyn std::error::Error>> {
-    info!("Listing reports");
-    let page = match find_option_value(&option.options[..], "page") {
-        Some(page) => page.as_i64().unwrap(),
-        None => 1,
+        Err(err) => return Err(err),
     };
 
-    let page_size =
-        find_option_value(&option.options[..], "page-size").map(|v| v.as_i64().unwrap());
+    crate::discord::respond(ctx, output).await
+}
 
-    let member = find_option_value(&option.options[..], "member").map(|member_id| {
-        let member_dc_id = member_id.as_str().unwrap();
-        Member::find_by_discord_id(member_dc_id)
-            .map(|member| member.id())
-            .unwrap() /*Some(member.id())*/
-    });
+#[poise::command(slash_command, aliases("list"))]
+pub(crate) async fn list_reports(
+    ctx: Context<'_>,
+    #[description = "Page number"] page: Option<i64>,
+    #[description = "Page size"] page_size: Option<i64>,
+    #[description = "Member's ID"] member: Option<Member>,
+    #[description = "Published"] published: Option<bool>,
+) -> Result<(), Error> {
+    info!("Listing reports");
+    let page = page.unwrap_or(1);
 
-    let published = find_option_value(&option.options[..], "published")
-        .map(|published| published.as_bool().unwrap());
+    let member = member.map(|member| member.id());
 
     let (reports, total_pages) = Report::list(page, page_size, member, published)?;
 
@@ -124,38 +95,34 @@ pub(crate) fn list_reports(
 
     write!(&mut output, "Page {} of {}", page, total_pages)?;
 
-    Ok(output)
+    crate::discord::respond(ctx, output).await
 }
 
-pub(crate) fn update_report(
-    option: &CommandDataOption,
-) -> Result<String, Box<dyn std::error::Error>> {
+#[poise::command(slash_command, aliases("update"))]
+pub(crate) async fn update_report(
+    ctx: Context<'_>,
+    #[description = "Report's ID"] mut report: Report,
+    #[description = "Report's content"] content: Option<String>,
+    #[description = "Member's ID"] member: Option<Member>,
+) -> Result<(), Error> {
     info!("Updating report");
-    let mut old_report = match find_option_value(&option.options[..], "id") {
-        Some(report_id) => {
-            let report_id = Uuid::parse_str(report_id.as_str().unwrap())?;
-            if let Ok(report) = Report::find_by_id(report_id) {
-                report
-            } else {
-                return Ok("Can't find report with this ID".to_string());
-            }
-        }
-        None => return Ok("No report specified".to_string()),
+
+    if let Some(content) = content {
+        report.content = content;
+    }
+
+    if let Some(member) = member {
+        report.member_id = member.id();
+    }
+
+    let report = match report.update() {
+        Ok(report) => report,
+        Err(why) => return Err(anyhow!("Can't update report: {}", why)),
     };
-
-    if let Some(content) = find_option_value(&option.options[..], "content") {
-        old_report.content = String::from(content.as_str().unwrap());
-    }
-
-    if let Some(member) = find_option_value(&option.options[..], "member") {
-        let member_dc_id = member.as_str().unwrap();
-        let member = Member::find_by_discord_id(member_dc_id)?;
-        old_report.member_id = member.id();
-    }
-
-    let report = old_report.update()?;
 
     info!("Report updated: {:?}", report);
 
-    Ok(format!("Report updated: {}", report))
+    let output = format!("Report updated: {}", report);
+
+    crate::discord::respond(ctx, output).await
 }
