f977e1d694299051ad7f6c0c9fc07b45f51dcd64
SLASHLogin <loginmlgxd@gmail.com>
2023-08-25 12:11:01 +0200

    Add mutation to create users in wiki

--- src/wiki/mod.rs
+++ src/wiki/mod.rs
@@ -20,7 +20,14 @@ pub struct AssignUserGroup;
 )]
 pub struct UnassignUserGroup;
 
-pub async fn assign_user_group(variables: assign_user_group::Variables) -> Result<(), Error> {
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "wiki/schema.graphql",
+    query_path = "wiki/mutations/create_user.graphql"
+)]
+pub struct CreateUser;
+
+fn get_client() -> Result<reqwest::Client, Error> {
     let mut headers = header::HeaderMap::new();
 
     let mut auth_value = header::HeaderValue::from_static(&SETTINGS.wiki.token);
@@ -31,6 +38,12 @@ pub async fn assign_user_group(variables: assign_user_group::Variables) -> Resul
         .user_agent("octobot/".to_owned() + env!("CARGO_PKG_VERSION"))
         .default_headers(headers)
         .build()?;
+
+    Ok(client)
+}
+
+pub async fn assign_user_group(variables: assign_user_group::Variables) -> Result<(), Error> {
+    let client = get_client()?;
 
     let body = AssignUserGroup::build_query(variables);
 
@@ -61,16 +74,7 @@ pub async fn assign_user_group(variables: assign_user_group::Variables) -> Resul
 }
 
 pub async fn unassign_user_group(variables: unassign_user_group::Variables) -> Result<(), Error> {
-    let mut headers = header::HeaderMap::new();
-
-    let mut auth_value = header::HeaderValue::from_static(&SETTINGS.wiki.token);
-    auth_value.set_sensitive(true);
-    headers.insert(header::AUTHORIZATION, auth_value);
-
-    let client = reqwest::Client::builder()
-        .user_agent("octobot/".to_owned() + env!("CARGO_PKG_VERSION"))
-        .default_headers(headers)
-        .build()?;
+    let client = get_client()?;
 
     let body = UnassignUserGroup::build_query(variables);
 
@@ -92,6 +96,35 @@ pub async fn unassign_user_group(variables: unassign_user_group::Variables) -> R
         .unwrap()
         .response_result
         .unwrap();
+
+    if response_result.succeeded {
+        Ok(())
+    } else {
+        Err(anyhow!(response_result.message.unwrap()))
+    }
+}
+
+pub async fn create_user(variables: create_user::Variables) -> Result<(), Error> {
+    let client = get_client()?;
+
+    let body = CreateUser::build_query(variables);
+
+    let res = client.post(&SETTINGS.wiki.url).json(&body).send().await?;
+
+    let response_body: graphql_client::Response<create_user::ResponseData> = res.json().await?;
+
+    if response_body.errors.is_some() {
+        return Err(anyhow!(response_body.errors.unwrap()[0].message.clone()));
+    }
+
+    let response_result = response_body
+        .data
+        .unwrap()
+        .users
+        .unwrap()
+        .create
+        .unwrap()
+        .response_result;
 
     if response_result.succeeded {
         Ok(())
